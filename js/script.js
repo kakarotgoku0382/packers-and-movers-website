92a57d4a3b2db67495d741b7d4c247a15a64e203
Tapas Ranjan Patra <kakarotgoku0382@gmail.com>
2025-07-30 15:49:48 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 {
-    fetch('../html/navbar.html')
+    fetch('html/navbar.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('navbar-container').innerHTML = data;
